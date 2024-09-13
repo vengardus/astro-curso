0a31787a77241853442cc2515964008dae245ddf
Ismytv <ismytv@gmail.com>
2024-09-12 22:45:22 -0500

    feat(section5):integracion markdow, mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,14 +8,13 @@ import solidJs from "@astrojs/solid-js";
 
 import react from "@astrojs/react";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon(), 
-    react({
-      include: ['**/react/**/*'],
-    }),
-    solidJs({
-      include: ['**/solid/*'],
-    }),
-],
+  integrations: [tailwind(), icon(), react({
+    include: ['**/react/**/*'],
+  }), solidJs({
+    include: ['**/solid/*'],
+  }), mdx()],
 });
