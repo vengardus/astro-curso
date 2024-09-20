192059796caca94c9080cf979d23c7b9b1748f64
Ismytv <ismytv@gmail.com>
2024-09-19 20:26:41 -0500

    feat(section6):relación de colecciones

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,22 +1,36 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection, reference, z } from "astro:content";
 
 const blogCollection = defineCollection({
-  schema: ({image}) => z.object({
-    title: z.string(),
-    date: z.date(),
-    description: z.string(),
-    image: image().refine((img)=> img.width < 1200, 'La imagen debe tener un ancho menor a 1200'),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      date: z.date(),
+      description: z.string(),
+      image: image().refine(
+        (img) => img.width < 1200,
+        "La imagen debe tener un ancho menor a 1200",
+      ),
 
-    // Relation
-    author: z.string(),
+      // Relation
+      // author: z.string(),
+      author: reference("author"),
 
-    // Relationm
-    tags: z.array(z.string()),
+      // Relationm
+      tags: z.array(z.string()),
+    }),
+});
+
+const authorCollection = defineCollection({
+  type: "data",
+  schema: ({ image }) => z.object({
+    name: z.string(),
+    avatar: image(),
   }),
 });
 
 // Debe tene el nombre collectiions
 // crear carpeta contents/blog
-export const collections = { 
-    blog: blogCollection 
-}
+export const collections = {
+  blog: blogCollection,
+  author: authorCollection,
+};
